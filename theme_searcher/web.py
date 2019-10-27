9055e2d98c7c10477a633134430ef679c31dffb7
GoodInc <igor.s.stulikov@gmail.com>
2019-10-27 13:23:48 +0300

    implemented web

--- theme_searcher/web.py
+++ theme_searcher/web.py
@@ -1,11 +1,37 @@
 from sanic import Sanic, response
 from sanic.exceptions import abort
 
+from .searcher import Searcher
+
 app = Sanic(__name__)
+
+
+@app.listener('before_server_start')
+async def init_searcher(app, loop):
+    """Builds searcher indexes when app started"""
+    # Searcher is built here to ensure lazy load when app starts,
+    # not during module import
+    searcher = Searcher({
+        'новости': ['деревья на Садовом кольце', 'добрый автобус',
+                    'выставка IT-технологий'],
+        'кухня': ['рецепт борща', 'яблочный пирог', 'тайская кухня'],
+        'товары': ['Дети капитана Гранта', 'зимние шины', 'Тайская кухня'],
+    })
+
+    # I can implement searcher asynchronously but decided to separate 
+    # implementation details and business rules. So I need async wrapper
+    async def search(text):
+        return searcher(text)
+
+    app.search = search
 
 
 @app.route("/search")
 async def test(request):
-    if 'text' not in request.args:
+    try:
+        text = request.args['text']
+    except KeyError:
         abort(400)
-    return response.json({"test": True})
+    else:
+        res = await app.search(text[0])
+        return response.json(res)
